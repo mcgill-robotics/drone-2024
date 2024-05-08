5138020ddd36e8a1930079483b42a893743cb53a
UnHappySquid <imad.issafras@mail.mcgill.ca>
2024-05-08 00:11:00 -0400

    organizing scripts and added new node to sim starter script

--- onboard_ws/qol_scripts/sim_tmux_start_script.sh
+++ onboard_ws/qol_scripts/sim_tmux_start_script.sh
@@ -19,7 +19,7 @@ then
         tmux split-window -t "$SESSION:$window" -t 2 -h
 
         tmux send-keys -t "$SESSION:$window" -t 0 'cd' enter 'cd PX4-Autopilot' enter
-        tmux send-keys -t "$SESSION:$window" -t 0 'make px4_sitl gazebo-classic' enter
+        tmux send-keys -t "$SESSION:$window" -t 0 'make px4_sitl gz_x500' enter
 
         tmux send-keys -t "$SESSION:$window" -t 1 'MicroXRCEAgent udp4 -p 8888' enter
 
@@ -28,6 +28,8 @@ then
         tmux send-keys -t "$SESSION:$window" -t 2 'ros2 run px4_offboard_control offboard_node' enter
 
         tmux send-keys -t "$SESSION:$window" -t 3 'source install/setup.bash' enter
+
+        tmux send-keys -t "$SESSION:$window" -t 3 'ros2 run ros_gazebo_bridge lidar_bridge' enter
 fi
 # Attach Session, on the Main window
 tmux attach-session -t $SESSION
--- onboard_ws/testing_scripts/qc_waypoint_then_silence_test.sh
+++ onboard_ws/testing_scripts/qc_waypoint_then_silence_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./install/setup.bash
+source ../install/setup.bash
 
 ## Takeoff
 ros2 service call /px4_action_queue/append_action custom_msgs/srv/SendAction "{action: {action: 1, x: 0., y: 0., z: 0.}}"
