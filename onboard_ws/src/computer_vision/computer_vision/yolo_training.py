eb1104fa38dd2e006390a17830f385151c8a59ad
Bessonov Vladimir Evgenievich <bessonov.vladimir1204@gmail.com>
2024-06-21 21:56:12 -0400

    added weights with one epoch

--- onboard_ws/src/computer_vision/computer_vision/yolo_training.py
+++ onboard_ws/src/computer_vision/computer_vision/yolo_training.py
@@ -1,8 +1,7 @@
 from ultralytics import YOLO
+if __name__ == "__main__":
+    #load model
+    model = YOLO("yolov8n.yaml")  #build new model from scratch
 
-#load model
-model = YOLO("yolov8n.yaml")  #build new model from scratch
-
-#use the model
-results = model.train(data='config.yaml', epochs=1)  #train the model
-
+    #use the model
+    results = model.train(data='C:\\Users\\besso\\tmp\\shapeandhumans.v2i.yolov8\\data.yaml', epochs=1)  #train the model
