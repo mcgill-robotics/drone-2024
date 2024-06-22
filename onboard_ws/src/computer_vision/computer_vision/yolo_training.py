091faa9c2c0db2624ebe8cdd9f023361ad24f8c0
Bessonov Vladimir Evgenievich <bessonov.vladimir1204@gmail.com>
2024-06-21 22:37:31 -0400

    new weights!!!!

--- onboard_ws/src/computer_vision/computer_vision/yolo_training.py
+++ onboard_ws/src/computer_vision/computer_vision/yolo_training.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     model = YOLO("yolov8n.yaml")  #build new model from scratch
 
     #use the model
-    results = model.train(data='C:\\Users\\besso\\tmp\\shapeandhumans.v2i.yolov8\\data.yaml', epochs=1)  #train the model
+    results = model.train(data='C:\\Users\\besso\\tmp\\shapeandhumans.v2i.yolov8\\data.yaml', epochs=20)  #train the model
