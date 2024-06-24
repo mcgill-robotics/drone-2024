2ff6f7d094c9409d4d53911b4a960d192a927b05
UnHappySquid <imad.issafras@mail.mcgill.ca>
2024-06-24 09:08:23 -0400

    started working on camera script modifications, aksi reduced the rate at which the vehicle info is published to put less strain on the system

--- onboard_ws/computer_vision_script/camera_video_v2.py
+++ onboard_ws/computer_vision_script/camera_video_v2.py
@@ -2,7 +2,9 @@ import cv2
 import time
 
 #index of camera device which is 0 with one camera & DOESNT WORK WITHOUT SECOND PART
-camera = cv2.VideoCapture(0, cv2.CAP_V4L2)
+camera = cv2.VideoCapture(
+   "nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM),width=1920,height=1080,framerate=30/1 ! nvvidconv ! video/x-raw, format=I420 ! queue ! autovideosink",
+   cv2.CAP_GSTREAMER)
 
 if (camera.isOpened()):
    print("The camera is successfully opened")
@@ -10,9 +12,9 @@ else:
    print("Could not open the camera")
    exit()
 
-width = int(640 * 3.6)
-height = int(480 * 3.6)
-fr = 5
+width = int(640)
+height = int(480)
+# fr = 5
 # ok = camera.set(cv2.CAP_PROP_MODE, cv2.CAP_MODE_GRAY)
 # if (not ok):
 #    print("Could not change camera mode")
@@ -26,10 +28,10 @@ if (not ok):
    print("Could not change image height")
    exit()
 #
-ok = camera.set(cv2.CAP_PROP_FPS, fr)
-if (not ok):
-   print("Could not change camera frame rate")
-   exit()
+# ok = camera.set(cv2.CAP_PROP_FPS, fr)
+# if (not ok):
+#    print("Could not change camera frame rate")
+#    exit()
 
 time.sleep(2)
 frameWidth = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -39,15 +41,15 @@ print(f"w, h, fr: {frameWidth}, {frameHeight}, {frameRate}")
 
 # for .avi files use DIVX, XVID
 # for .mp4 files use MJPG
-fourccCode = cv2.VideoWriter_fourcc(*'DIVX')
+# fourccCode = cv2.VideoWriter_fourcc(*'DIVX')
 
-videoFileName = 'recordedVideo.avi'  #put whtvr video file name is
-
-videoDimension = (frameWidth, frameHeight)  # defines dimensions of video
-
-recordedVideo = cv2.VideoWriter(
-   videoFileName, fourccCode, frameRate,
-   videoDimension)  # creates a Video Writer object
+# videoFileName = './recordedVideo.avi'  #put whtvr video file name is
+#
+# videoDimension = (frameWidth, frameHeight)  # defines dimensions of video
+#
+# recordedVideo = cv2.VideoWriter(
+#    videoFileName, fourccCode, frameRate,
+#    videoDimension)  # creates a Video Writer object
 
 while True:
    # captures the frame
@@ -64,6 +66,6 @@ while True:
    if cv2.waitKey(1) == ord('c'):  # if press c the video capturing will end
       break
 
-recordedVideo.release()
+# recordedVideo.release()
 camera.release()
 cv2.destroyAllWindows()
