b2f556591f810ede3ec3f23ceed0ac6586e22407
UnHappySquid <imad.issafras@mail.mcgill.ca>
2024-06-26 01:24:33 -0400

    removed useless files, added gitignore

--- onboard_ws/src/computer_vision/computer_vision/alphanumeric_trainer_yolo.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-#load model
-model = YOLO("yolov8n.yaml") #build new model from scratch
-
-#use the model
-results = model.train(data='data.yaml', epochs=20) #train the model
